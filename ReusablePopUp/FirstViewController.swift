a1e3abf799bd750368c09804efc66321b3d9f29d
Marcos <marcos.suarez@rocketmail.com>
2017-12-03 08:28:21 +0100

    Initial Commit

---
+++ ReusablePopUp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ReusablePopUp
+//
+//  Created by Marcos Suarez on 3/12/17.
+//  Copyright © 2017 Marcos Suarez. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ReusablePopUp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ReusablePopUp
+//
+//  Created by Marcos Suarez on 3/12/17.
+//  Copyright © 2017 Marcos Suarez. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
